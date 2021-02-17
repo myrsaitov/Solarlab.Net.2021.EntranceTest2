2116673be73fb7092719736f72a50d35824e5664
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-02-17 17:17:44 +0300

    Content Contracts, Interface and Services were added

---
+++ Application/Content/Contracts/Delete.cs
@@ -0,0 +1,15 @@
+﻿namespace WidePictBoard.Application.Content.Contracts
+{
+    public static class Delete
+    {
+        public sealed class Request
+        {
+            int Id { get; set; }
+        }
+        
+        public sealed class Response
+        {
+            
+        }
+    }
+}
--- Infrastructure/Services/Content/ContentService.cs
+++ Infrastructure/Services/Content/ContentService.cs
@@ -1,5 +1,7 @@
 ﻿using WidePictBoard.Application;
-using WidePictBoard.Application.Content.Service;
+using WidePictBoard.Application.Content.Interface;
+using System.Threading.Tasks;
+using WidePictBoard.Application.Content.Contracts;
 
 namespace WidePictBoard.Infrastructure.Services.Content
 {
@@ -11,5 +13,32 @@ namespace WidePictBoard.Infrastructure.Services.Content
         {
             _repository = repository;
         }
+        
+        
+        public async Task<GetById.Response> GetById(GetById.Request request)
+        {
+            throw new System.NotImplementedException();
+        }
+        public async Task<GetPaged.Response> GetPaged(GetPaged.Request request)
+        {
+            throw new System.NotImplementedException();
+        }
+        public async Task<Delete.Response> Delete(Delete.Request request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Create.Response> Create(Create.Request request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Update.Response> Update(Update.Request request)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        
+        
     }
 }
