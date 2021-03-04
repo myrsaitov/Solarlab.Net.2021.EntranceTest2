d53519a1d95082bce7303f0507fd5dd8de4c6a88
mirkm <mirkm@tavrida.com>
2021-03-04 18:01:39 +0300

    Comment Contracts were added

---
+++ Application/Comment/Contracts/CreateComment.cs
@@ -0,0 +1,16 @@
+﻿namespace WidePictBoard.Application.Comment.Contracts
+{
+    public static class CreateComment
+    {
+        public sealed class Request
+        {
+            public int AdId { get; set; }
+            public string Text { get; set; }
+        }
+
+        public sealed class Response
+        {
+            public int Id { get; set; }
+        }
+    }
+}
---
+++ Application/Comment/Contracts/GetPagedComments.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WidePictBoard.Application.Comment.Contracts
+{
+    public static class GetPagedComments
+    {
+        public sealed class Request
+        {
+            public int Offset { get; set; }
+            public int Limit { get; set; }
+        }
+
+        public sealed class Response
+        {
+            public sealed class Item
+            {
+                public int AdId { get; set; }
+                public int Id { get; set; }
+
+                public int AuthorId { get; set; }
+                public string AuthorName { get; set; }
+                public string Text { get; set; }
+            }
+
+            public int Total { get; set; }
+            public int Offset { get; set; }
+            public int Limit { get; set; }
+
+            public IEnumerable<Item> Items { get; set; } = Enumerable.Empty<Item>();
+        }
+    }
+}
