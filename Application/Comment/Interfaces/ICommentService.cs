650f1d59aaae0910d2dd4d016dd173cbf7796828
mirkm <mirkm@tavrida.com>
2021-03-04 18:03:09 +0300

    Comment interface was added

---
+++ Application/Comment/Interfaces/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WidePictBoard.Application.Comment.Contracts;
+
+namespace Advertisement.Application.Services.Comment.Interfaces
+{
+    public interface ICommentService
+    {
+        Task<CreateComment.Response> CreateComment(CreateComment.Request request, CancellationToken cancellationToken);
+
+        Task<GetPagedComments.Response> GetPagedComments(GetPagedComments.Request request,
+            CancellationToken cancellationToken);
+    }
+}
