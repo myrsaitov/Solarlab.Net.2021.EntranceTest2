9909b2a780d28db23f09844c1f0c9302c808b21e
mirkm <mirkm@tavrida.com>
2021-03-04 18:18:45 +0300

    ITokenGenerator was added JWTTokenGenerator was added in infrastructure

---
+++ Application/User/Interfaces/ITokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WidePictBoard.Application.User.Interfaces
+{
+    public interface ITokenGenerator
+    {
+        Task<string> ObtainTokenFromClaims(IReadOnlyCollection<Claim> claims, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Identity/JwtTokenGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WidePictBoard.Application.User.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WidePictBoard.Infrastructure.Identity
+{
+    public sealed class JwtTokenGenerator : ITokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> ObtainTokenFromClaims(IReadOnlyCollection<Claim> claims, CancellationToken cancellationToken)
+        {
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(60),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:Key"])),
+                    SecurityAlgorithms.HmacSha256)
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
