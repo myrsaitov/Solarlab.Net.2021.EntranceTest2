cd0e79bbfeb9180bbe7fa5c27846ca444972aef9
mirkm <mirkm@tavrida.com>
2021-03-04 15:33:14 +0300

    Added app content exception

---
+++ Application/Content/Contracts/Exceptions/NoUserForContentCreationException.cs
@@ -0,0 +1,11 @@
+﻿using WidePictBoard.Domain.General.Exceptions;
+
+namespace WidePictBoard.Application.Content.Contracts.Exceptions
+{
+    public sealed class NoUserForContentCreationException : NoRightsException
+    {
+        public NoUserForContentCreationException(string message) : base(message)
+        {
+        }
+    }
+}
