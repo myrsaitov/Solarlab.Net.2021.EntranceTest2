2bdf2cf625a4637071fc657547182dbfc62e9604
mirkm <mirkm@tavrida.com>
2021-03-04 18:16:04 +0300

    Infrastructure HttpContextClaimsAccessor added

---
+++ Infrastructure/Identity/HttpContextClaimsAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using WidePictBoard.Application.User.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Advertisement.Infrastructure.Identity
+{
+    public sealed class HttpContextClaimsAccessor : IClaimsAccessor
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        public HttpContextClaimsAccessor(IHttpContextAccessor contextAccessor) => _contextAccessor = contextAccessor;
+        public async Task<IEnumerable<Claim>> GetCurrentClaims(CancellationToken cancellationToken) => _contextAccessor.HttpContext.User.Claims;
+    }
+}
