e6e4c30585e456d05e3287f35c5d1a68aee54ce7
mirkm <mirkm@tavrida.com>
2021-03-09 17:33:55 +0300

    DBContents

--- Infrastructure/DataAccess/DatabaseContext.cs
+++ Infrastructure/DataAccess/DatabaseContext.cs
@@ -1,11 +1,14 @@
-﻿using System.Xml.Xsl;
+﻿using WidePictBoard.Application.Common;
 using WidePictBoard.Domain;
 using WidePictBoard.Infrastructure.DataAccess.EntitiesConfiguration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using IdentityUser = WidePictBoard.Infrastructure.Identity.IdentityUser;
 
 namespace WidePictBoard.Infrastructure.DataAccess
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext : IdentityDbContext<IdentityUser>
     {
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
         {
@@ -13,7 +16,7 @@ namespace WidePictBoard.Infrastructure.DataAccess
         }
 
         public DbSet<Content> Contents { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<User> DomainUsers { get; set; }
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -22,6 +25,59 @@ namespace WidePictBoard.Infrastructure.DataAccess
             modelBuilder.ApplyConfiguration(new UserConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
 
+            SeedIdentity(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
         }
+
+        private void SeedIdentity(ModelBuilder modelBuilder)
+        {
+            var ADMIN_ROLE_ID = "d3300ca5-846f-4e6b-ac5f-1d3933115e67";
+            var ADMIN_ID = "98b651ae-c9aa-4731-9996-57352d525f7e";
+            var USER_ROLE_ID = "185230d2-58d8-4e29-aefd-a257fb82a150";
+
+            modelBuilder.Entity<IdentityRole>(x =>
+            {
+                x.HasData(new[]
+                {
+                    new IdentityRole
+                    {
+                        Id = ADMIN_ROLE_ID,
+                        Name = RoleConstants.AdminRole,
+                        NormalizedName = "ADMIN"
+                    },
+                    new IdentityRole
+                    {
+                        Id = USER_ROLE_ID,
+                        Name = RoleConstants.UserRole,
+                        NormalizedName = "USER"
+                    }
+                });
+            });
+
+            var passwordHasher = new PasswordHasher<IdentityUser>();
+            var adminUser = new IdentityUser
+            {
+                Id = ADMIN_ID,
+                UserName = "admin",
+                NormalizedUserName = "ADMIN"
+            };
+            adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "admin");
+
+            modelBuilder.Entity<IdentityUser>(x =>
+            {
+                x.HasData(adminUser);
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(x =>
+            {
+                x.HasData(new IdentityUserRole<string>
+                {
+                    RoleId = ADMIN_ROLE_ID,
+                    UserId = ADMIN_ID
+                });
+            });
+        }
+
     }
 }
