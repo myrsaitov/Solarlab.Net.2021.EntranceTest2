eb4844d5dfc2cef6e24a6f54366ab1ff731e6e2a
mirkm <mirkm@tavrida.com>
2021-03-09 17:12:52 +0300

    Services.Content.Contracts. GetById

--- Application/Services/Content/Contracts/GetById.cs
+++ Application/Services/Content/Contracts/GetById.cs
@@ -1,8 +1,8 @@
-﻿using WidePictBoard.Domain;
+﻿using System.Collections.Generic;
+using WidePictBoard.Domain;
 
 namespace WidePictBoard.Application.Services.Content.Contracts
 {
-
     public static class Pay
     {
         public sealed class Request
@@ -10,23 +10,22 @@ namespace WidePictBoard.Application.Services.Content.Contracts
             public int Id { get; set; }
         }
     }
+
     public static class GetById
     {
         public sealed class Request
         {
             public int Id { get; set; }
-            
         }
-        
+
         public sealed class Response
         {
             public sealed class OwnerResponse
             {
-                public int Id { get; set; }
+                public string Id { get; set; }
                 public string Name { get; set; }
             }
 
-            public string Name { get; set; }
             public string Status { get; set; }
             public decimal Price { get; set; }
 
