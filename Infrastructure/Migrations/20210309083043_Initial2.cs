0bd3b987a0e2a5e99e09787036dc8cd38275cfb2
mirkm <mirkm@tavrida.com>
2021-03-09 12:02:05 +0300

    Connection String UPD

---
+++ Infrastructure/Migrations/20210309083043_Initial2.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WidePictBoard.Infrastructure.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ads_Users_OwnerId",
+                table: "Ads");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ads",
+                table: "Ads");
+
+            migrationBuilder.RenameTable(
+                name: "Ads",
+                newName: "Contents");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Ads_OwnerId",
+                table: "Contents",
+                newName: "IX_Contents_OwnerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contents",
+                table: "Contents",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contents_Users_OwnerId",
+                table: "Contents",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contents_Users_OwnerId",
+                table: "Contents");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contents",
+                table: "Contents");
+
+            migrationBuilder.RenameTable(
+                name: "Contents",
+                newName: "Ads");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Contents_OwnerId",
+                table: "Ads",
+                newName: "IX_Ads_OwnerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ads",
+                table: "Ads",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ads_Users_OwnerId",
+                table: "Ads",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
