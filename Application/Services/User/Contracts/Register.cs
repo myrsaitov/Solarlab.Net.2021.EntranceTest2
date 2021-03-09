50b3b30cb20b9fea32b23e28c0d19542ca092a29
mirkm <mirkm@tavrida.com>
2021-03-09 17:24:12 +0300

    User contracts

--- Application/Services/User/Contracts/Register.cs
+++ Application/Services/User/Contracts/Register.cs
@@ -4,13 +4,16 @@
     {
         public sealed class Request
         {
-            public string Name { get; set; }
+            public string Username { get; set; }
             public string Password { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string MiddleName { get; set; }
         }
 
         public sealed class Response
         {
-            public int UserId { get; set; }
+            public string UserId { get; set; }
         }
     }
 }
---
+++ Application/Services/User/Contracts/Update.cs
@@ -0,0 +1,13 @@
+﻿namespace WidePictBoard.Application.Services.User.Contracts
+{
+    public static class Update
+    {
+        public class Request
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string MiddleName { get; set; }
+        }
+    }
+}
