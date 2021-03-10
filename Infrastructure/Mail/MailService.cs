2889fc771e9ef4a23a99b6f745670bcacca89ef3
mirkm <mirkm@tavrida.com>
2021-03-10 11:58:43 +0300

    Mail Infrastructure was added

---
+++ Infrastructure/Mail/MailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WidePictBoard.Application.Services.Mail.Interfaces;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace WidePictBoard.Infrastructure.Mail
+{
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _settings;
+
+        public MailService(IOptions<MailSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public async Task Send(string recipient, string subject, string message, CancellationToken cancellationToken = default)
+        {
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress("AdvertSite", _settings.Address));
+            mimeMessage.To.Add(new MailboxAddress("", recipient));
+            mimeMessage.Subject = subject;
+            mimeMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(_settings.Host, _settings.Port, _settings.UseSsl, cancellationToken);
+            await client.AuthenticateAsync(_settings.Address, _settings.Password, cancellationToken);
+            await client.SendAsync(mimeMessage, cancellationToken);
+            await client.DisconnectAsync(true, cancellationToken);
+        }
+    }
+}
