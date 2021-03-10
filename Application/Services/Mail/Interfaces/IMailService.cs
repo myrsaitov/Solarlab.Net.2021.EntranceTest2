ff1b9d8a15da0837ed54cf3ab06b52ada955b695
mirkm <mirkm@tavrida.com>
2021-03-10 11:23:09 +0300

    IMailService was added

---
+++ Application/Services/Mail/Interfaces/IMailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WidePictBoard.Application.Services.Mail.Interfaces
+{
+    public interface IMailService
+    {
+        Task Send(string recipient, string subject, string message, CancellationToken cancellationToken = default);
+    }
+}
