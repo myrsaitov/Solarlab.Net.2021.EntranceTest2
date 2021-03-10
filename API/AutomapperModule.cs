0c5934b0fc64b31178be3689917aea3c6ab45a11
mirkm <mirkm@tavrida.com>
2021-03-10 12:03:26 +0300

    AutomapperModule added

---
+++ API/AutomapperModule.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WidePictBoard.PublicApi
+{
+    public static class AutomapperModule
+    {
+        public static IServiceCollection AddAutomapperModule(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper>(new Mapper(GetMapperConfiguration()));
+            return services;
+        }
+
+        private static MapperConfiguration GetMapperConfiguration()
+        {
+            var configuration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<WidePictBoard.Application.MapProfiles.UserMapProfile>();
+            });
+
+            configuration.AssertConfigurationIsValid();
+
+            return configuration;
+        }
+    }
+}
