ae56cbef16e527ba0dc6e4f6e4775f500530e77d
mirkm <mirkm@tavrida.com>
2021-03-11 10:12:37 +0300

    /Application/Services/Content/Contracts/Create.cs

--- Application/Services/Content/Contracts/Create.cs
+++ Application/Services/Content/Contracts/Create.cs
@@ -13,24 +13,5 @@
         {
             public int Id { get; set; }
         }
-
-        /*       public sealed class Request
-        {
-            public string Title { get; set; }
-
-            public string Body { get; set; }
-
-            public string Email { get; set; }
-
-            public bool Deleted { get; set; }
-
-            public int CategoryId { get; set; }
-        }
-        
-        public sealed class Response
-        {
-           // int Id { get; set; }
-
-        }*/
     }
 }
