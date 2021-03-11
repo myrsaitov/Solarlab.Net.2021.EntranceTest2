c9b8fff15dce01227c00f8261f822aa027be2bee
mirkm <mirkm@tavrida.com>
2021-03-11 10:17:08 +0300

    /API/AutomapperModule.cs

--- Infrastructure/AutomapperModule.cs
+++ Infrastructure/AutomapperModule.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WidePictBoard.PublicApi
+namespace WidePictBoard.Infrastructure
 {
     public static class AutomapperModule
     {
