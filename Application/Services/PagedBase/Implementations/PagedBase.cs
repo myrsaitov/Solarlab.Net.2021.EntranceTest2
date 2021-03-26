c33eed97e45142f60c6f7897909c608958aacaaf
mirkm <mirkm@tavrida.com>
2021-03-26 17:57:50 +0300

    Base_ClassPaged_middle

---
+++ Application/Services/PagedBase/Implementations/PagedBase.cs
@@ -0,0 +1,60 @@
+﻿using MapsterMapper;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WidePictBoard.Application.Common;
+using WidePictBoard.Application.Identity.Interfaces;
+using WidePictBoard.Application.Repositories;
+using WidePictBoard.Application.Services.PagedBase.Contracts;
+using WidePictBoard.Application.Services.PagedBase.Interfaces;
+using WidePictBoard.Domain.General;
+using WidePictBoard.Domain.General.Exceptions;
+
+
+namespace WidePictBoard.Application.Services.PagedBase.Implementations
+{
+    public class PagedBase<TEntity>
+        : IPagedBase<TEntity>
+        where TEntity : EntityMutable<int>
+    {
+        private readonly IRepository<TEntity, int> _repository;
+        private readonly IIdentityService _identityService;
+        private readonly IMapper _mapper;
+        public PagedBase(IRepository<TEntity, int> repository, IIdentityService identityService, IMapper mapper)
+        {
+            _repository = repository;
+            _identityService = identityService;
+            _mapper = mapper;
+        }
+        public async Task<Paged.Response<TEntity>> GetPaged(Paged.Request request, CancellationToken cancellationToken)
+        {
+            var total = await _repository.Count(
+                cancellationToken
+            );
+
+            if (total == 0)
+            {
+                return new GetPaged.Response
+                {
+                    Items = Array.Empty<GetPaged.Response.ContentResponse>(),
+                    Total = total,
+                    Offset = request.Page,
+                    Limit = request.PageSize
+                };
+            }
+
+            var contents = await _repository.GetPaged(
+                request.Page, request.PageSize, cancellationToken
+            );
+
+            return new GetPaged.Response
+            {
+                Items = contents.Select(content => _mapper.Map<GetPaged.Response.ContentResponse>(content)),
+                Total = total,
+                Offset = request.Page,
+                Limit = request.PageSize
+            };
+        }
+    }
+}
--- Application/Services/PagedBase/Interfaces/IPagedBase.cs
+++ Application/Services/PagedBase/Interfaces/IPagedBase.cs
@@ -1,11 +1,15 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using WidePictBoard.Application.Repositories;
 using WidePictBoard.Application.Services.Content.Contracts;
+using WidePictBoard.Application.Services.PagedBase.Contracts;
+using WidePictBoard.Domain.General;
 
 namespace WidePictBoard.Application.Services.PagedBase.Interfaces
 {
     public interface IPagedBase<TEntity>
+    where TEntity : EntityMutable<int>
     {
-        Task<TEntity> GetPaged(TEntity request, CancellationToken cancellationToken);
+        Task<Paged.Response<TEntity>> GetAll(Paged.Request request, CancellationToken cancellationToken);
     }
 }
