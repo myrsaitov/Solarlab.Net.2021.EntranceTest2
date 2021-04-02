dbcf1c333a0fdb2806eaf70b703df77b4b14b6a3
mirkm <mirkm@tavrida.com>
2021-04-02 09:43:14 +0300

    TagConfig changed

--- Infrastructure/DataAccess/EntitiesConfiguration/TagConfiguration.cs
+++ Infrastructure/DataAccess/EntitiesConfiguration/TagConfiguration.cs
@@ -7,45 +7,6 @@ namespace WidePictBoard.Infrastructure.DataAccess.EntitiesConfiguration
 {
     public class TagConfiguration : IEntityTypeConfiguration<Tag>
     {
-        private Tag[] tags = new Tag[]
-        {
-            new Tag
-            {
-                Id = 1,
-                Body = "tag1",
-                CreatedAt = DateTime.UtcNow
-            },
-            new Tag
-            {
-                Id = 2,
-                Body = "tag2",
-                CreatedAt = DateTime.UtcNow
-            },
-            new Tag
-            {
-                Id = 3,
-                Body = "tag3",
-                CreatedAt = DateTime.UtcNow
-            },
-            new Tag
-            {
-                Id = 4,
-                Body = "tag4",
-                CreatedAt = DateTime.UtcNow
-            },
-            new Tag
-            {
-                Id = 5,
-                Body = "tag5",
-                CreatedAt = DateTime.UtcNow
-            },
-            new Tag
-            {
-                Id = 6,
-                Body = "tag6",
-                CreatedAt = DateTime.UtcNow
-            }
-        };
         public void Configure(EntityTypeBuilder<Tag> builder)
         {
             builder.HasKey(t => t.Id);
