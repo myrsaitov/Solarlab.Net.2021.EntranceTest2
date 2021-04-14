5dd3d7671965c384d4c1279d27adbc8f5da178e1
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-04-14 23:41:49 +0300

    GetById Test Added

--- Tests/Content/ContentServiceV1Test.GetById.cs
+++ Tests/Content/ContentServiceV1Test.GetById.cs
@@ -24,61 +24,11 @@ namespace WidePictBoard.Tests.Content
             int categoryId)
         {
             // Arrange
-            ConfigureMoqForGetByIdMethod(
-                userId.ToString(), 
-                contentTitle, 
-                contentBody, 
-                tagBodies, 
-                categoryId);
-
-            // Act
-            var response = await _contentServiceV1.GetById(
-                request, 
-                cancellationToken);
-
-            // Assert
-            _contentRepositoryMock.Verify();
-            Assert.NotNull(response);
-            Assert.NotEqual(default, response.Id);
-        }
-        [Theory]
-        [InlineAutoData(null)]
-        public async Task GetById_Throws_Exception_When_Request_Is_Null(
-            GetById.Request request, 
-            CancellationToken cancellationToken, 
-            int userId,
-            string contentTitle,
-            string contentBody,
-            string[] tagBodies,
-            int categoryId)
-        {
-            // Arrange
-            ConfigureMoqForGetByIdMethod(
-                userId.ToString(), 
-                contentTitle, 
-                contentBody, 
-                tagBodies, 
-                categoryId);
-
-            // Act
-            await Assert.ThrowsAsync<ArgumentNullException>(
-                async () => await _contentServiceV1.GetById(
-                    request, 
-                    cancellationToken));
-
-        }
-        private void ConfigureMoqForGetByIdMethod(
-            string userId, 
-            string contentTitle, 
-            string contentBody, 
-            string[] tagBodies, 
-            int categoryId)
-        {
             var content = new Domain.Content()
             {
                 Title = contentTitle,
                 Body = contentBody,
-                OwnerId = userId,
+                OwnerId = userId.ToString(),
                 Category = new Domain.Category()
                 {
                     Id = categoryId
@@ -99,11 +49,45 @@ namespace WidePictBoard.Tests.Content
 
             _contentRepositoryMock
                 .Setup(_ => _.FindByIdWithUserAndCategoryAndTags(
-                    It.IsAny<int>(), 
+                    It.IsAny<int>(),
                     It.IsAny<CancellationToken>()))
                 .ReturnsAsync(content)
                 .Callback((int _contentId, CancellationToken ct) => content.Id = _contentId)
                 .Verifiable();
+
+            // Act
+            var response = await _contentServiceV1.GetById(
+                request, 
+                cancellationToken);
+
+            // Assert
+            _contentRepositoryMock.Verify();
+            Assert.NotNull(response);
+            Assert.NotEqual(default, response.Id);
+        }
+        [Theory]
+        [AutoData]
+        public async Task GetById_Throws_Exception_When_Content_Is_Null(
+            GetById.Request request,
+            CancellationToken cancellationToken)
+        {
+            // Act
+            await Assert.ThrowsAsync<ContentNotFoundException>(
+                async () => await _contentServiceV1.GetById(
+                    request,
+                    cancellationToken));
+        }
+        [Theory]
+        [InlineAutoData(null)]
+        public async Task GetById_Throws_Exception_When_Request_Is_Null(
+            GetById.Request request, 
+            CancellationToken cancellationToken)
+        {
+            // Act
+            await Assert.ThrowsAsync<ArgumentNullException>(
+                async () => await _contentServiceV1.GetById(
+                    request, 
+                    cancellationToken));
         }
     }
 }
