a86aca7df5cbf2ccb40701fa845aa07de320d1b5
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-04-14 23:51:48 +0300

    Content Test Delete

--- Tests/Content/ContentServiceV1Test.Delete.cs
+++ Tests/Content/ContentServiceV1Test.Delete.cs
@@ -1,11 +1,12 @@
 ﻿using WidePictBoard.Application.Services.Content.Contracts;
-using WidePictBoard.Application.Services.Content.Contracts.Exceptions;
 using Moq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using AutoFixture.Xunit2;
 using System;
+using WidePictBoard.Application.Services.Content.Contracts.Exceptions;
+using WidePictBoard.Domain.General.Exceptions;
 
 namespace WidePictBoard.Tests.Content
 {
@@ -20,37 +21,60 @@ namespace WidePictBoard.Tests.Content
             int contentId)
         {
             // Arrange
-            ConfigureMoqForDeleteMethod(userId.ToString(), contentId);
+            var content = new Domain.Content()
+            {
+                OwnerId = userId.ToString()
+            };
+
+            _contentRepositoryMock
+                .Setup(_ => _.FindByIdWithUserInclude(
+                    It.IsAny<int>(), 
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(content)
+                .Callback((int _contentId, CancellationToken ct) => content.Id = _contentId)
+                .Verifiable();
+
+            _identityServiceMock
+                .Setup(_ => _.GetCurrentUserId(
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(userId.ToString())
+                .Verifiable();
+
+            _identityServiceMock
+                .Setup(_ => _.IsInRole(
+                    It.IsAny<string>(), 
+                    It.IsAny<string>(), 
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false)
+                .Verifiable();
+
+            _contentRepositoryMock
+                .Setup(_ => _.Save(
+                    It.IsAny<Domain.Content>(), 
+                    It.IsAny<CancellationToken>()))
+                .Callback((Domain.Content content, CancellationToken ct) => content.Id = contentId);
 
             // Act
-            await _contentServiceV1.Delete(request, cancellationToken);
+            await _contentServiceV1.Delete(
+                request, 
+                cancellationToken);
 
             // Assert
             _identityServiceMock.Verify();
             _contentRepositoryMock.Verify();
         }
         [Theory]
-        [InlineAutoData(null)]
-        public async Task Delete_Throws_Exception_When_Request_Is_Null(
-            Delete.Request request, 
-            CancellationToken cancellationToken, 
-            int userId, 
-            int contentId
-            )
+        [AutoData]
+        public async Task Delete_Throws_Exception_When_No_Rights(
+    Delete.Request request,
+    CancellationToken cancellationToken,
+    int userId,
+    int contentId)
         {
             // Arrange
-            ConfigureMoqForDeleteMethod(userId.ToString(), contentId);
-
-            // Act
-            await Assert.ThrowsAsync<ArgumentNullException>(
-                async () => await _contentServiceV1.Delete(request, cancellationToken));
-
-        }
-        private void ConfigureMoqForDeleteMethod(string userId, int contentId)
-        {
             var content = new Domain.Content()
             {
-                OwnerId = userId
+                OwnerId = userId.ToString()
             };
 
             _contentRepositoryMock
@@ -61,17 +85,52 @@ namespace WidePictBoard.Tests.Content
 
             _identityServiceMock
                 .Setup(_ => _.GetCurrentUserId(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(userId)
+                .ReturnsAsync("")
                 .Verifiable();
 
             _identityServiceMock
-                .Setup(_ => _.IsInRole(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Setup(_ => _.IsInRole(
+                    It.IsAny<string>(), 
+                    It.IsAny<string>(), 
+                    It.IsAny<CancellationToken>()))
                 .ReturnsAsync(false)
                 .Verifiable();
 
             _contentRepositoryMock
-                .Setup(_ => _.Save(It.IsAny<Domain.Content>(), It.IsAny<CancellationToken>()))
+                .Setup(_ => _.Save(
+                    It.IsAny<Domain.Content>(), 
+                    It.IsAny<CancellationToken>()))
                 .Callback((Domain.Content content, CancellationToken ct) => content.Id = contentId);
+
+            // Act
+            await Assert.ThrowsAsync<NoRightsException>(
+                async () => await _contentServiceV1.Delete(
+                    request, 
+                    cancellationToken));
+        }
+        [Theory]
+        [AutoData]
+        public async Task Delete_Throws_Exception_When_Content_Is_Null(
+            Delete.Request request,
+            CancellationToken cancellationToken)
+        {
+            // Act
+            await Assert.ThrowsAsync<ContentNotFoundException>(
+                async () => await _contentServiceV1.Delete(
+                    request, 
+                    cancellationToken));
+        }
+        [Theory]
+        [InlineAutoData(null)]
+        public async Task Delete_Throws_Exception_When_Request_Is_Null(
+            Delete.Request request, 
+            CancellationToken cancellationToken)
+        {
+            // Act
+            await Assert.ThrowsAsync<ArgumentNullException>(
+                async () => await _contentServiceV1.Delete(
+                    request, 
+                    cancellationToken));
         }
     }
 }
