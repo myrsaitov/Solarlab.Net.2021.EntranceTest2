cfaf61a35c8ade1df3244a6d83efec36f58754c6
mirkm <mirkm@tavrida.com>
2021-05-06 13:06:09 +0300

    Tags Count Fixed

--- Application/MapProfiles/ContentMapProfile.cs
+++ Application/MapProfiles/ContentMapProfile.cs
@@ -11,12 +11,12 @@ namespace SL2021.Application.MapProfiles
 
             config.NewConfig<Domain.Content, Services.Content.Contracts.GetById.Response>()
                 .Map(dest => dest.CreatedAt, src => src.CreatedAt.ToLocalTime().ToString("dd/MM/yy H:mm:ss (zzz)"))
-                .Map(dest => dest.Tags, src => src.Tags.Select(a => a.Body + '(' + a.Count + ')').ToArray());
+                .Map(dest => dest.Tags, src => src.Tags.Select(a => a.Body).ToArray());
 
             config.NewConfig<Domain.Content, Services.Content.Contracts.GetPaged.Response>()
                 .Map(dest => dest.CreatedAt, src => src.CreatedAt.ToLocalTime().ToString("dd/MM/yy H:mm:ss (zzz)"))
                 .Map(dest => dest.CategoryName, src => src.Category.Name)
-                .Map(dest => dest.Tags, src => src.Tags.Select(a => a.Body +'('+ a.Count + ')').ToList());
+                .Map(dest => dest.Tags, src => src.Tags.Select(a => a.Body).ToArray());
             return config;
         }
     }
