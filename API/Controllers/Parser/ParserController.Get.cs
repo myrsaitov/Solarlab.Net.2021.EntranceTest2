54c914ce332c4e357a0e92bdace7a354f255d414
Konstantin Myrsaitov <myrsaitov@gmail.com>
2021-05-22 15:26:22 +0300

    Parsing XML

---
+++ API/Controllers/Parser/ParserController.Get.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using SL2021.Application.Services.Content.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SL2021.Application.Services.Contracts;
+using System.Linq;
+using HtmlAgilityPack; //nuget HtmlAgilityPack
+using TurnerSoftware.SitemapTools; ///nuget TurnerSoftware.SitemapTools
+using System.Collections.Generic;
+
+namespace SL2021.API.Controllers.Parser
+{
+    public partial class ParserController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(
+            string Text, 
+            CancellationToken cancellationToken)
+        {
+
+            var ress = new List<string>();
+
+            var sitemapQuery = new SitemapQuery();
+            var sitemapEntries = await sitemapQuery.GetAllSitemapsForDomainAsync(Text);
+
+
+            return Ok(ress);
+
+/*			var doc = new HtmlAgilityPack.HtmlDocument();
+
+			HtmlWeb web = new HtmlWeb();
+
+            var ress = new List<string>();
+
+            doc = web.Load("https://stroypark.su/");
+            string res = doc.DocumentNode.SelectSingleNode($"//*[text()[contains(., '{Text}')]]").InnerText;
+            if (ress is not null)
+            {
+                ress.Add(res);
+            }
+            else
+            {
+                ress.Add("null");
+            }
+
+            doc = web.Load("https://petrovich.ru/");
+            res = doc.DocumentNode.SelectSingleNode($"//*[text()[contains(., '{Text}')]]").InnerText;
+            if (ress is not null)
+            {
+                ress.Add(res);
+            }
+            else
+            {
+                ress.Add("null");
+            }
+
+            doc = web.Load("https://www.vseinstrumenti.ru/");
+            res = doc.DocumentNode.SelectSingleNode($"//*[text()[contains(., '{Text}')]]").InnerText;
+            if (ress is not null)
+            {
+                ress.Add(res);
+            }
+            else
+            {
+                ress.Add("null");
+            }
+
+            return Ok(ress);*/
+        }
+    }
+}
---
+++ API/Controllers/Parser/ParserController.cs
@@ -0,0 +1,23 @@
+﻿// @nuget: HtmlAgilityPack
+
+using System;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SL2021.Application.Services.Content.Interfaces;
+
+namespace SL2021.API.Controllers.Parser
+{
+    [Route("api/v1/parser")]
+    [ApiController]
+    [Authorize]
+    public partial class ParserController : ControllerBase
+    {
+        private readonly IContentService _contentService;
+        public ParserController(IContentService contentService)
+        {
+            _contentService = contentService;
+        }
+    }
+}
